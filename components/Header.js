a1d56336e24e125a25457b3084bed2934354fc70
erintibroca48-ops <erintibroca48@gmail.com>
2026-06-10 17:49:40 +0200

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -19,7 +19,7 @@ export default function Header({ search, setsearch }) {
 
     const t = {
         sq: {
-            home: "Home",
+            home: "He",
             products: "Produkte",
             custom: "Personalizime",
             contact: "Kontakto",
