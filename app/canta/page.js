ee2484e77273781fbaba8185ead6cba3b807dd75
erintibroca48-ops <erintibroca48@gmail.com>
2026-06-10 18:36:50 +0200

    Update page.js

--- app/canta/page.js
+++ app/canta/page.js
@@ -127,7 +127,7 @@ export default function Canta() {
                                 <div
                                     key={index}
                                     onClick={() => setSelectedIndex(index)}
-                                    className="w-40 md:w-60 overflow-hidden shadow-lg cursor-pointer group items-center justify-center"
+                                    className="flex flex-col items-center justify-center overflow-hidden shadow-lg cursor-pointer group mx-auto w-full max-w-[240px]"
                                 >
                                     <img
                                         src={img}
